2c50ec955351a72cb860253ee0255a73379f95b9
Pradeep K. P <prap@lab.morganstanley.com>
2023-07-05 10:46:33 +0000

    adde blocker

--- WEB/Ajmal SSRF/run.sh
+++ WEB/Ajmal SSRF/run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
+iptables -A OUTPUT -d 169.254.169.254 -j REJECT
 python3 -m http.server 3301 --bind 127.0.0.1 --directory /app/flag &
 P1=$!
 node app.js
 P2=$!
-iptables -A OUTPUT -d 169.254.169.254 -j REJECT
-P3=$!
-wait $P1 $P2 $P3
+wait $P1 $P2
