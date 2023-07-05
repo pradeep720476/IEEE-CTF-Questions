29e0b32f08976e9782f785ae1ae6e37061dced6d
Pradeep K. P <prap@lab.morganstanley.com>
2023-07-05 13:15:39 +0000

    add

--- WEB/Ajmal SSRF/run.sh
+++ WEB/Ajmal SSRF/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+apt-get update && \
+apt-get -y install sudo
 sudo iptables -I FORWARD -i docker0 -d 169.254.169.254 \ -p tcp -m multiport --dports 80,443 -j REJECT 
 python3 -m http.server 3301 --bind 127.0.0.1 --directory /app/flag &
 P1=$!
