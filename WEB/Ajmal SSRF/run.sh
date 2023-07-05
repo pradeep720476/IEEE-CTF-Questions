c1a03fd49cf27230cc5205b76f7ff876fc34a95a
Pradeep K. P <prap@lab.morganstanley.com>
2023-07-05 13:11:31 +0000

    add

--- WEB/Ajmal SSRF/run.sh
+++ WEB/Ajmal SSRF/run.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-
+sudo iptables -I FORWARD -i docker0 -d 169.254.169.254 \ -p tcp -m multiport --dports 80,443 -j REJECT 
 python3 -m http.server 3301 --bind 127.0.0.1 --directory /app/flag &
 P1=$!
 node app.js
 P2=$!
-sudo iptables -I FORWARD -i docker0 -d 169.254.169.254 \ -p tcp -m multiport --dports 80,443 -j REJECT 
-P3=$!
-wait $P1 $P2 $P3
+wait $P1 $P2
