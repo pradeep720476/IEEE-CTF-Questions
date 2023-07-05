e9abf611c6b65c2e3ecb43867e0a7ba332773327
Pradeep K. P <prap@lab.morganstanley.com>
2023-07-05 13:37:32 +0000

    add

--- WEB/Ajmal SSRF/run.sh
+++ WEB/Ajmal SSRF/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-iptables -I FORWARD -i docker0 -d 169.254.169.254 \ -p tcp -m multiport --dports 80,443 -j REJECT 
+iptables -I FORWARD -i docker0 -d 169.254.169.254 -j REJECT 
 python3 -m http.server 3301 --bind 127.0.0.1 --directory /app/flag &
 P1=$!
 node app.js
